ac659f9071ac269e7e127534265916472dd9961c
Zhustas <zhongli3137@gmail.com>
2023-10-30 00:01:59 +0200

    Login fxml

--- src/main/java/com/shop/StartGUI.java
+++ src/main/java/com/shop/StartGUI.java
@@ -12,7 +12,7 @@ public class StartGUI extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartGUI.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login to shop");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/shop/controllers/LoginController.java
+++ src/main/java/com/shop/controllers/LoginController.java
@@ -1,5 +1,15 @@
 package com.shop.controllers;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
+public class LoginController {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    public void printInformation(){
+        System.out.println(username.getText() + " " + password.getText());
+    }
 }
