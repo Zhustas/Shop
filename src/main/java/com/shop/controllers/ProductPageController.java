39a62b72d14abed51ca9e889f0354f19f0e581a2
Zhustas <zhongli3137@gmail.com>
2023-11-03 11:10:27 +0200

    Product Page Controller

--- src/main/java/com/shop/controllers/ProductPageController.java
+++ src/main/java/com/shop/controllers/ProductPageController.java
@@ -1,21 +1,69 @@
 package com.shop.controllers;
 
 import com.shop.Utils.Utils;
+import com.shop.classes.Product;
 import com.shop.classes.User;
+import com.shop.classes.Warehouse;
 import jakarta.persistence.EntityManagerFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProductPageController {
+public class ProductPageController implements Initializable {
     @FXML
     private AnchorPane anchorPane;
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextField manufacturerField;
+    @FXML
+    private TextField priceField;
+    @FXML
+    private TextArea descriptionField;
+    @FXML
+    private TableView<Product> productsTable;
+    @FXML
+    private ListView<Warehouse> warehouseList;
+
     private EntityManagerFactory entityManagerFactory;
     private User user;
+
     public void setData(EntityManagerFactory entityManagerFactory, User user){
         this.entityManagerFactory = entityManagerFactory;
         this.user = user;
+    }
+
+    @FXML
+    private void createProduct(){
+
+    }
+
+    @FXML
+    private void updateProduct(){
+
+    }
+
+    @FXML
+    private void deleteProduct(){
+
+    }
+
+    @FXML
+    private void clearFields(){
+        titleField.setText(null);
+        manufacturerField.setText(null);
+        priceField.setText(null);
+        descriptionField.setText(null);
+        warehouseList.getSelectionModel().clearSelection();
+    }
+
+    private void loadProductsTable(){
+        // Load all products from database
     }
 
     @FXML
@@ -25,5 +73,11 @@ public class ProductPageController {
     @FXML
     private void loadAccountPage() throws IOException {
         Utils.loadAccountPage(entityManagerFactory, user, anchorPane);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        warehouseList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        loadProductsTable();
     }
 }
